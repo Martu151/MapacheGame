b3f0b3ec29e1b8fd9517eb950386f40706f895a4
Martu151 <martuselem@gmail.com>
2024-01-27 19:05:23 -0300

    fgfadgdgd

---
+++ Source/MapacheGame/Public/Jugador/MyMapache.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MyMapache.generated.h"
+
+UCLASS()
+class MAPACHEGAME_API AMyMapache : public ACharacter
+{
+	GENERATED_BODY()
+	
+public:
+//Variables
+UPROPERTY(EditAnywhere, BlueprintReadWrite) int movSpeed;
+UPROPERTY(BlueprintReadWrite) bool tieneObjeto;
+UPROPERTY(BlueprintReadWrite) bool miraDerecha;
+
+//Funciones
+void VerticalAxis(float valor);
+void HorizontalAxis(float valor);
+//UFUNCTION(BlueprintCallable) void AccionAgarra(AMyObjetoAgarrable* obj);
+
+public:
+	// Sets default values for this character's properties
+	AMyMapache();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};

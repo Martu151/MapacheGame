d8c14ac9ddd6c909f9012c7fea8c23582fe46172
Martu151 <martuselem@gmail.com>
2024-01-27 19:04:12 -0300

    dsfdgd

--- Source/MapacheGame/MapacheGame.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "MapacheGame.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MapacheGame, "MapacheGame" );
- 
